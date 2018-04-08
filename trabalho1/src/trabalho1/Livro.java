6facb0370602d6b4fad7b7282f97cd4632d7ded1
lrm5060 <leonardo_rm_@hotmail.com>
2018-04-08 16:45:16 -0300

    Classe Livro

---
+++ trabalho1/src/trabalho1/Livro.java
@@ -0,0 +1,54 @@
+package trabalho1;
+
+public class Livro {
+	
+	private int codigo;
+	private String nome;
+	private String descricao;
+	private int anoFabricacao;
+	private int anoValidade;
+	private int numeroEmprestimos;
+	
+	public Livro(int codigo, String nome, String descricao, int anoFabricacao, int anoValidade, int numeroEmprestimos) {
+		this.codigo = codigo;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.anoFabricacao = anoFabricacao;
+		this.anoValidade = anoValidade;
+		this.numeroEmprestimos = numeroEmprestimos;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public int getAnoFabricacao() {
+		return anoFabricacao;
+	}
+
+	public int getAnoValidade() {
+		return anoValidade;
+	}
+
+	public int getNumeroEmprestimos() {
+		return numeroEmprestimos;
+	}
+
+	public void setNumeroEmprestimos(int numeroEmprestimos) {
+		this.numeroEmprestimos = numeroEmprestimos;
+	}
+
+	@Override
+	public String toString() {
+		return "Livro [codigo=" + codigo + ", nome=" + nome + ", descricao=" + descricao + ", anoFabricacao="
+				+ anoFabricacao + ", anoValidade=" + anoValidade + ", numeroEmprestimos=" + numeroEmprestimos + "]";
+	}	
+}
